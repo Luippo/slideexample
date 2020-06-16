25adfb28f4e7e6b1d71948a4f988e9bdcd38f74f
luis.ramos <luis.ramos@solex.biz>
2020-06-15 21:19:05 -0400

    fin del ejercicio de slide

--- js/slide.js
+++ js/slide.js
@@ -4,17 +4,39 @@ var p = {
     item: 0,
     cajaSlide: document.querySelector("#slide ul"),
     animacionSlide: "slide",
-    imgSlide: document.querySelectorAll("#slide ul li")
+    imgSlide: document.querySelectorAll("#slide ul li"),
+    avanzar: document.querySelector("#slide #avanzar"),
+    retroceder: document.querySelector("#slide #retroceder")
 }
 // Objeto con Metodos del Sline
 var m = {
     inicioSlide: function(){
         for(var i = 0; i < p.paginacion.length; i++){
             p.paginacion[i].addEventListener("click",m.paginacionSlide)
-        }        
+        }
+        p.avanzar.addEventListener("click",m.avanzar);
+        p.retroceder.addEventListener("click",m.retroceder);
     },
     paginacionSlide: function(item){
         p.item = item.target.parentNode.getAttribute("item") - 1; // ParentNode, sirve para llamar al padre.
+        m.movimientoSlide(p.item);
+    },
+    avanzar: function(){        
+        if(p.item == p.imgSlide.length - 1){
+            p.item = 0;
+        }else{
+            p.item++;
+        }
+        console.log(p.item);
+        m.movimientoSlide(p.item);
+    },
+    retroceder: function(){
+        if(p.item == 0){
+            p.item = p.imgSlide.length - 1;
+        }else{
+            p.item--;
+        }
+        console.log(p.item);
         m.movimientoSlide(p.item);
     },
     movimientoSlide: function(item){        
